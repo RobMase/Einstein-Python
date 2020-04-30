3704f1dd665b752b5213b67f0586a0346d5d8407
unknown <robert.masur@live.com>
2020-04-30 23:04:14 +0200

    adding a little turtle

---
+++ 01/01_turtle.py
@@ -0,0 +1,16 @@
+import turtle
+
+bildschirm = turtle.getscreen()
+
+schildkroete = turtle.Turtle()
+
+schildkroete.shapesize(5,5,5)
+schildkroete.forward(500)
+schildkroete.left(90)
+schildkroete.forward(500)
+schildkroete.left(90)
+schildkroete.forward(500)
+schildkroete.left(90)
+schildkroete.forward(1000)
+
+turtle.done()
