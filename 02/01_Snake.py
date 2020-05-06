82ad813695121eed6fa36f892ac7676ed909b9fb
unknown <robert.masur@live.com>
2020-05-06 21:52:07 +0200

    Add new conent

---
+++ 02/01_Snake.py
@@ -0,0 +1,17 @@
+import turtle
+
+schildi = turtle.Turtle()
+
+#schildi.shapesize(3,3,3)
+#schildi.shape("turtle") # “arrow”, “turtle”, “circle”, “square”, “triangle”, “classic”
+#schildi.speed(1) # 1 - 10 (0)
+schildi.forward(200)
+#schildi.left(90) # in grad (0 - 360)
+#schildi.back(100)
+#schildi.right(90)
+#schildi.forward(150)
+schildi.dot(20)
+schildi.forward(200)
+schildi.circle(100)
+
+turtle.done()
