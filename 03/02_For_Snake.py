47df6597a41e1a91441614fd07f5a19a4978914a
unknown <robert.masur@live.com>
2020-05-13 21:34:02 +0200

    add lecture 3

---
+++ 03/02_For_Snake.py
@@ -0,0 +1,20 @@
+import turtle
+
+schildi = turtle.Turtle()
+
+schildi.shapesize(1,1,1)
+schildi.shape("turtle") # “arrow”, “turtle”, “circle”, “square”, “triangle”, “classic”
+schildi.speed(3) # 1 - 10 (0)
+
+for index in range(4):
+    schildi.forward(200)
+    schildi.left(90) # in grad (0 - 360)
+else:
+    schildi.dot(50)
+
+schildi.setpos(-300, 0)
+for index in range(8):
+    schildi.left(45)
+    schildi.forward(200)
+
+turtle.done()
---
+++ 03/03_While_Schleife.py
@@ -0,0 +1,13 @@
+index = 10
+
+while index > 0:
+    print(index)
+    index = index - 1
+else:
+    print("Ende der While-Schleife.")
+
+# eingabe = ""
+# while eingabe != "Hoi":
+#     eingabe = input("Bitte gib 'Hoi' ein: ")
+# else:
+#     print("Danke!")
