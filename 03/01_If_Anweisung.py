c202d690622322e690fd82ee0f194a95a635d6f1
unknown <robert.masur@live.com>
2020-05-15 22:01:31 +0200

    adding if expression

---
+++ 03/01_If_Anweisung.py
@@ -0,0 +1,40 @@
+esIstWarm = False
+
+if esIstWarm:
+    print("Zieh die kurzen Hosen an.")
+print ("Bye bye")
+
+
+# if esIstWarm:
+#     print("Zieh die kurzen Hosen an.")
+#     print("Und trinke viel Wasser.")
+# print ("Bye bye")
+
+# if esIstWarm:
+#     print("Zieh die kurzen Hosen an.")
+#     print("Und trinke viel Wasser.")
+# else:
+#     print("Dann zieh dich warm an")
+#     print("Und trinke warmen Tee")
+# print ("Bye bye")
+
+esIstKalt = True
+
+# if esIstWarm:
+#     print("Zieh die kurzen Hosen an.")
+#     print("Und trinke viel Wasser.")
+# else: 
+#     if esIstKalt:
+#         print("Dann zieh dich warm an.")
+#         print("Und trinke warmen Tee.")
+# print ("Bye bye")
+
+if esIstWarm:
+    print("Zieh die kurzen Hosen an.")
+    print("Und trinke viel Wasser.")
+elif esIstKalt:
+    print("Dann zieh dich warm an.")
+    print("Und trinke warmen Tee.")
+else:
+    print("Geniesse den schönen Tag.")
+print ("Bye bye")
