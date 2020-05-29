a89ddae9ffcb894d7d297b36656b18ecdc385465
unknown <robert.masur@live.com>
2020-05-29 23:21:26 +0200

    Add set

---
+++ 05/05_Mengen.py
@@ -0,0 +1,11 @@
+liste = [1,1,1,1,1]
+menge = {1,1,1,1,1}
+
+print(f"Liste: {liste}")
+print(f"Menge: {menge}")
+
+liste = [23,342,1,5,8,20,-50]
+print(f"Unsortierte Liste: {liste}")
+print(f"Sortiere meine Liste: {sorted(liste)}")
+neue_liste = liste.sort()
+print(f"Sortiere meine Liste: {neue_liste}")
